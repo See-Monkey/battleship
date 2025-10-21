37971e22458b4e045a157b6ae2644e37c73e85c5
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-10-20 19:43:04 -0600

    Add placeShip and testPlaceShip functions to Gameboard

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -1,8 +1,60 @@
 // 10 x 10 board
-// Vertical axis 1 - 10
-// Horizontal axis A - J
+// Vertical axis A - J
+// Horizontal axis 1 - 10
 // Origin upper left
+// Ship orentation = 0-horizontal, 1-vertical
+
+import Ship from "./ship.js";
 
 export default class Gameboard {
-  constructor() {}
+  constructor() {
+    this.board = [];
+    for (let i = 0; i < 10; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < 10; j++) {
+        this.board[i][j] = null;
+      }
+    }
+
+    this.ships = {
+      carrier: new Ship("carrier"),
+      battleship: new Ship("battleship"),
+      cruiser: new Ship("cruiser"),
+      submarine: new Ship("submarine"),
+      destroyer: new Ship("destroyer"),
+    };
+  }
+
+  placeShip(shipName, shipOrigin, shipOrientation) {
+    let vert = shipOrigin[0];
+    let horiz = shipOrigin[1];
+
+    const ship = this.ships[shipName];
+    const shipLength = ship.length;
+
+    if (this.testPlaceShip(shipLength, shipOrigin, shipOrientation) === false)
+      return;
+
+    for (let i = 0; i < shipLength; i++) {
+      this.board[vert][horiz] = shipName;
+      shipOrientation === 0 ? horiz++ : vert++;
+    }
+  }
+
+  testPlaceShip(shipLength, shipOrigin, shipOrientation) {
+    let vert = shipOrigin[0];
+    let horiz = shipOrigin[1];
+
+    for (let i = 0; i < shipLength; i++) {
+      if (
+        this.board[vert] === undefined ||
+        this.board[vert][horiz] === undefined ||
+        this.board[vert][horiz] !== null
+      )
+        return false;
+
+      shipOrientation === 0 ? horiz++ : vert++;
+    }
+    return true;
+  }
 }
--- src/js/gameboard.test.js
+++ src/js/gameboard.test.js
@@ -1,5 +1,44 @@
 import Gameboard from "./gameboard.js";
 
-test("test", () => {
-  expect(1).toBe(1);
+let gameboard;
+
+beforeEach(() => {
+  gameboard = new Gameboard();
+});
+
+describe("Gameboard Array", () => {
+  test("array of arrays", () => {
+    expect(gameboard.board[9][9]).toBeNull();
+  });
+
+  test("size of array", () => {
+    expect(gameboard.board[10]).toBeUndefined();
+  });
+});
+
+describe("Gameboard Place Ship", () => {
+  test("place ship horizontal", () => {
+    gameboard.placeShip("destroyer", [0, 0], 0);
+    expect(gameboard.board[0][1]).toBe("destroyer");
+  });
+
+  test("place ship vertical", () => {
+    gameboard.placeShip("submarine", [0, 1], 1);
+    expect(gameboard.board[2][1]).toBe("submarine");
+  });
+});
+
+describe("Gameboard Test Place Ship", () => {
+  test("test place ship horizontal", () => {
+    expect(gameboard.testPlaceShip(3, [0, 0], 0)).toBe(true);
+  });
+
+  test("test place ship vertical", () => {
+    gameboard.placeShip("destroyer", [0, 1], 1);
+    expect(gameboard.testPlaceShip(3, [0, 1], 1)).toBe(false);
+  });
+
+  test("test place ship out of bounds", () => {
+    expect(gameboard.testPlaceShip(3, [0, 8], 0)).toBe(false);
+  });
 });
