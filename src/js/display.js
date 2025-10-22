cbe6d1cf5f74db9315e5128a250a4159c905558a
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-10-21 22:34:57 -0600

    Start display class and pregame dialog

--- src/js/display.js
+++ src/js/display.js
@@ -1,3 +1,33 @@
+import Player from "./player.js";
+
 export default class Display {
-  constructor() {}
+  constructor() {
+    this.player1;
+    this.player2;
+    this.activePlayer = this.player1;
+    this.inactivePlayer = this.player2;
+  }
+
+  createElement(tag, className, id) {
+    const element = document.createElement(tag);
+    element.className = className;
+    if (id !== undefined) element.id = id;
+  }
+
+  init() {
+    const dialog = document.querySelector("dialog");
+    dialog.showModal();
+  }
+
+  start() {
+    // validate data
+    // take values from the dom
+    // initialize players
+    // redraw and await input
+  }
+
+  redraw() {
+    content = document.querySelector(".content");
+    content.innerHTML = "";
+  }
 }
