ba7802414699a5537d9be88959439e6216d62605
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-11-01 20:10:35 -0600

    Add testing for Player module

--- src/js/player.test.js
+++ src/js/player.test.js
@@ -10,29 +10,77 @@ beforeEach(() => {
 });
 
 describe("Random Attack", () => {
-  test("array of options", () => {
-    expect().toBeNull();
-  });
-
   test("random selection", () => {
-    expect().toBeUndefined();
+    // Spy on opponent’s receiveAttack to ensure it’s called with a valid coordinate
+    const spy = jest.spyOn(player2.gameboard, "receiveAttack");
+
+    player1.randomAttack();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    const [[vert, horiz]] = spy.mock.calls[0];
+    expect(vert).toBeGreaterThanOrEqual(0);
+    expect(vert).toBeLessThanOrEqual(9);
+    expect(horiz).toBeGreaterThanOrEqual(0);
+    expect(horiz).toBeLessThanOrEqual(9);
   });
 });
 
 describe("Search and Destroy", () => {
   test("mode activation", () => {
-    expect().toBeNull();
+    player2.gameboard.placeShip("Destroyer", [0, 0], 0);
+
+    // Simulate an attack that hits
+    player1.opponent = player2;
+    player1.randomAttack = jest.fn(() => {
+      player1.opponent.gameboard.receiveAttack([0, 0]);
+      player1.previousResult = "hit";
+      player1.search = 1;
+    });
+
+    player1.randomAttack();
+
+    // Expect search mode to activate
+    expect(player1.search).toBe(1);
+    expect(player1.previousResult).toBe("hit");
   });
 
   test("correct targeting", () => {
-    expect().toBeUndefined();
+    // Place a ship for player1 at [0,0] horizontally
+    player1.gameboard.placeShip("Destroyer", [0, 0], 0);
+
+    // Set up computer to begin search mode
+    player2.search = 1;
+    player2.searchCoordinate = "0,0";
+    player2.searchDirection = [0, 1]; // east
+
+    // Spy on attacks
+    const spy = jest.spyOn(player1.gameboard, "receiveAttack");
+
+    player2.searchAndDestroy();
+
+    // Check that the next coordinate attacked was adjacent east ([0,1])
+    const [[vert, horiz]] = spy.mock.calls[0];
+    expect([[vert, horiz]]).toEqual([[0, 1]]);
   });
 
   test("direction change after miss", () => {
-    expect().toBeNull();
+    player1.gameboard.placeShip("Destroyer", [0, 0], 0);
+    player1.gameboard.receiveAttack([0, 1]);
+    player2.search = 1;
+    player2.searchCoordinate = "0,1";
+    player2.searchDirection = [0, 1];
+    player2.searchAndDestroy();
+    expect(player2.searchDirection).toEqual([0, -1]);
   });
 
   test("disengage after sunk", () => {
-    expect().toBeUndefined();
+    player1.gameboard.placeShip("Destroyer", [0, 0], 0);
+    player1.gameboard.receiveAttack([0, 0]);
+    player2.search = 1;
+    player2.searchCoordinate = "0,0";
+    player2.searchDirection = [0, 1];
+    player2.searchAndDestroy();
+    expect(player2.search).toBe(0);
   });
 });
